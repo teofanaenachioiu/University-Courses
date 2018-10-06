5602c30cd809e2f457e8d821910a4dbf2a558b7f
Teofana <teofanaenachioiu@yahoo.com>
2018-10-06 20:49:40 +0300

    added a validation function of a complex number

--- MAP/Laborator1/src/Main.java
+++ MAP/Laborator1/src/Main.java
@@ -1,7 +1,42 @@
 public class Main {
 
     private static boolean isComplexNumber(String nr){
-        return true;
+        if(nr.charAt(nr.length()-1)!='i') {
+            //cazul in care numarul are doar parte reala
+            if (nr.charAt(0)=='-'){
+                //numarul e negativ; elimin semnul din fata
+                nr=nr.substring(1);
+            }
+            try {
+                //verific daca numarul e intreg
+                Integer.parseInt(nr);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        else {
+            //cazul in care numarul are si parte imaginara
+            //elimin i-ul de la final
+            nr=nr.substring(0,nr.length()-1);
+            if(nr.charAt(0)=='-'){
+                //daca numarul incepe cu semn, elimin semnul
+                nr=nr.substring(1);
+            }
+            //Split dupa - sau +
+            String[] parts = nr.split("[-+]");
+            try {
+                //cazul "bi"
+                Integer.parseInt(parts[0]);
+                if (parts.length==2) {
+                    //cazul "a+bi"
+                    Integer.parseInt(parts[1]);
+                }
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
     }
 
     private static boolean isOperator (String op){
@@ -30,14 +65,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        NumarComplex nrs=new NumarComplex("3i");
+        /*NumarComplex nrs=new NumarComplex("3i");
         System.out.println(nrs.toString());
 
         NumarComplex nrs1=new NumarComplex("25");
         System.out.println(nrs1.toString());
 
         NumarComplex nrs2=new NumarComplex("-100-9i");
-        System.out.println(nrs2.toString());
+        System.out.println(nrs2.toString());*/
 
         if(validation(args)==true) {
             System.out.println("Parametrii reprezinta o expresie aritmetica.");
