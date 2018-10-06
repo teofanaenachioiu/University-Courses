97ef686d75c5a5bb0a9298d6a617850f1edc21c7
Teofana <teofanaenachioiu@yahoo.com>
2018-10-06 22:36:57 +0300

    some changes in NumarComplex class

--- MAP/Laborator1/src/Main.java
+++ MAP/Laborator1/src/Main.java
@@ -1,42 +1,5 @@
 public class Main {
 
-    private static boolean isComplexNumber(String nr){
-        if(nr.charAt(nr.length()-1)!='i') {
-            //cazul in care numarul are doar parte reala
-            try {
-                //verific daca numarul e intreg
-                Integer.parseInt(nr);
-                return true;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }
-        else {
-            //cazul in care numarul are si parte imaginara
-            //elimin i-ul de la final
-            nr=nr.substring(0,nr.length()-1);
-            //Split dupa - sau +
-            String[] parts = nr.split("[-+]");
-            try {
-                //Numarul a inceput cu minu => la split parts[0]=""
-                if(!parts[0].equals("")) {
-                    Integer.parseInt(parts[0]);
-                }
-                if (parts.length==2) {
-                    System.out.println(parts[1]);
-                    Integer.parseInt(parts[1]);
-                }
-                if (parts.length==3) {
-                    System.out.println(parts[2]);
-                    Integer.parseInt(parts[2]);
-                }
-                return true;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }
-    }
-
     private static boolean isOperator (String op){
         String [] operators={"+","-","*","/"};
         for (String o: operators) {
@@ -52,10 +15,16 @@ public class Main {
 
         for(int i=0;i<args.length;i++){
             if(i%2==0){
-                if(isComplexNumber((args[i]))==false) return false;
+                try{
+                    NumarComplex nr=new NumarComplex(args[i]);
+                    System.out.println("Numarul complex este: "+nr.toString());
+                }
+                catch(NumberFormatException e){
+                    return false;
+                }
             }
             else{
-                if(isOperator(args[i])==false) return false;
+                if(!isOperator(args[i])) return false;
             }
         }
 
--- MAP/Laborator1/src/NumarComplex.java
+++ MAP/Laborator1/src/NumarComplex.java
@@ -6,60 +6,69 @@ public class NumarComplex {
         this.real = 0;
     }
 
-    public NumarComplex(String value)  {
+    public NumarComplex(String nr)  {
         real = 0;
         img = 0;
-        int i = value.length(); // length of current string to parse
+        if(nr.charAt(nr.length()-1)!='i') {
+            //cazul in care numarul are doar parte reala
+            real=Integer.parseInt(nr);
+        }
+        else {
+            boolean isNegative = false;
+            //cazul in care numarul are si parte imaginara
+            //elimin i-ul de la final
+            nr = nr.substring(0, nr.length() - 1);
 
-        // if "i" is last character(value has imaginary part)
-        if (value.charAt(value.length() - 1) == 'i') {
-            i = value.length() - 1;
-            img = 1;
-
-            // case "i"
-            if (value.length() == 1) {
-                return;
+            String[] parts = nr.split("[-+]");
+            for(String c:parts)System.out.println("|"+c+"|");
+            System.out.println("Lungimea lui parts "+parts.length);
+            if(parts.length==0) img=-1; //cazul -i
+            if (parts.length>0&&parts[0].equals("")) {
+                //Numarul a inceput cu minus => la split parts[0]=""
+                isNegative = true;
             }
-
-            // case "-xi" | "+xi" | "xi"
-            for (int j = i - 1; j >= 0; j--) {
-                if (value.charAt(j) == '-') {
-                    // case "-i"
-                    if (j == i - 1) {
-                        img *= -1;
-                        i = j;
-                        break;
+            if(parts.length==1){
+                //cazul bi
+                if(isNegative)img=1; //cazul i
+                else {
+                    //cazul a+i si a-i
+                    if(nr.charAt(nr.length()-1)=='-'||nr.charAt(nr.length()-1)=='+') {
+                        real=Integer.parseInt(parts[0]);
+                        if(nr.indexOf('-')>0)img=-1;
+                        else img=1;
                     }
-                    img = Integer.valueOf(value.substring(j, i));
-                    i = j;
-                    break;
-                } else if (value.charAt(j) == '+') {
-                    img = Integer.valueOf(value.substring(j, i));
-                    i = j;
-                    break;
-                }
-
-                // case "xi" (real part equals zero)
-                if (j == 0) {
-                    img = Integer.valueOf(value.substring(j, i));
-                    return;
+                    else img = Integer.parseInt(parts[0]);
                 }
             }
-
-        }
-
-        // for real part
-        if (i == 0) {
-            // cases "-xi" | "+xi", x != 0
-            if ((value.charAt(i) == '-' || value.charAt(i) == '+') & (img != 0)) {
-                return;
+            if (parts.length == 2) {
+                if (isNegative) {
+                    //cazul -bi
+                    if(parts[1].equals("")) img=1;
+                    else img = (-1)*Integer.parseInt(parts[1]);
+                }
+                else {
+                    //cazul a+bi
+                    if(nr.charAt(nr.length()-1)=='-'||nr.charAt(nr.length()-1)=='+') {
+                        real=Integer.parseInt(parts[1]);
+                        if(nr.indexOf('-')>0)img=-1;
+                        else img=1;
+                    }
+                    else{
+                        real = Integer.parseInt(parts[0]);
+                        if (parts[1].equals("")) img = 1;
+                        else img = Integer.parseInt(parts[1]);
+                        if (nr.indexOf('-') > 0) img = img * (-1);//cazul a-bi
+                    }
+                }
             }
-
-            real = Integer.valueOf(value);
-            return;
+            if (parts.length == 3) {
+                //cazul -a+bi
+                real = (-1)*Integer.parseInt(parts[1]);
+                if(parts[2].equals("")) img=1;
+                else img = Integer.parseInt(parts[2]);
+                if(nr.indexOf('-')>0)img=img*(-1);//cazul -a-bi
+            }
         }
-
-        real = Integer.valueOf(value.substring(0, i));
     }
 
     public int getImg() {
