fcf587605596d3c7c03b9d5c89387b4ee589e30b
Teofana <teofanaenachioiu@yahoo.com>
2018-10-07 18:41:39 +0300

    added lab1

---
+++ Databases/Laborator1/creareTabele.sql
@@ -0,0 +1,25 @@
+create database TabaraDeVara
+
+create table Insotitori(
+	Iid int primary key,
+	Nume varchar(30) not null,
+	Telefon varchar(10) default 'Set phone number'
+)
+
+create table Asociatii(
+	Aid int primary key,
+	Denumire varchar(30) not null
+)
+
+create table Grupe(
+	Gid int primary key,
+	Denumire varchar(40) not null  
+)
+
+create table Participanti(
+	Pid int primary key,
+	Aid int foreign key references Asociatii(Aid),
+	Gid int foreign key references Grupe(Gid),
+	Nume varchar(50) not null,
+	Data_n date not null
+)
