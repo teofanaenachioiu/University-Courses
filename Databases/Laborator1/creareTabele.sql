bda1ee2fed315bcf75477e9dee57114d56374443
Teofana <teofanaenachioiu@yahoo.com>
2018-10-09 00:23:24 +0300

    added items

--- Databases/Laborator1/creareTabele.sql
+++ Databases/Laborator1/creareTabele.sql
@@ -1,10 +1,5 @@
 create database TabaraDeVara
 
-drop table Insotitori
-drop table Asociatii
-drop table Grupe
-drop table Participanti
-
 create table Insotitori(
 	Iid int primary key,
 	Nume varchar(30) not null,
@@ -17,9 +12,17 @@ create table Asociatii(
 	constraint FK_Asociatii_Insotitori foreign key(Aid) references Insotitori(Iid)
 )
 
+create table Moderatori(
+	Mid int primary key,
+	Nume varchar(50) not null,
+	Telefon varchar(10) not null,
+	Data_n date default 'Set date'
+)
+
 create table Grupe(
 	Gid int primary key,
 	Denumire varchar(40) not null  
+	constraint FK_Grupe_Insotitori foreign key(Gid) references Moderatori(Mid)
 )
 
 create table Participanti(
@@ -28,4 +31,16 @@ create table Participanti(
 	Gid int foreign key references Grupe(Gid),
 	Nume varchar(50) not null,
 	Data_n date not null
+)
+
+create table Cazari(
+	Cid int primary key,
+	Locatie varchar(20) not null,
+	NrLocuri int 
+)
+
+create table Cereri(
+	Aid int foreign key references Asociatii(Aid),
+	Cid int foreign key references Cazari(Cid),
+	CONSTRAINT PK_Cereri primary key(Aid, Cid)
 )
