d8c0bb27b62ce7d5e6e48364b203d0677c6c20c9
Teofana <teofanaenachioiu@yahoo.com>
2018-10-12 13:43:34 +0300

    added files

---
+++ Databases/Seminar1/SQLQuery1.sql
@@ -0,0 +1,7 @@
+create database World;
+
+alter database World
+modify name=World24
+
+drop database World24
+
---
+++ Databases/Seminar1/SQLQuery2.sql
@@ -0,0 +1,11 @@
+create database Seminar1
+use Seminar1
+
+drop table Studenti
+
+CREATE TABLE Studenti(
+	cod_s int not null,
+	nume varchar(50) not null,
+	prenume varchar(50) not null,
+	localitate varchar(50) not null
+)
