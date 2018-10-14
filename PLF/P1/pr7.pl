2b553ae05a6e77b3a52fde83221f0b348fb6185d
Teofana <teofanaenachioiu@yahoo.com>
2018-10-14 23:26:17 +0300

    lab2 final

--- PLF/P1/pr7.pl
+++ PLF/P1/pr7.pl
@@ -20,3 +20,27 @@ reuniune([H|T],L2,LR):-apartine(L2,H),reuniune(T,L2,LR).
 reuniune([H|T],L2,[H|LR]):-not(apartine(L2,H)),reuniune(T,L2,LR).
 
 %determina multimea tutuor perechilor din lista
+%perechi(E:int,L:lista,LP:lista)
+%E - primul element din seria de perechi
+%L - lista de elemente cu care se acociaza elementul E
+%LP - lista de perechi
+%model de flux (i,i,o),(i,i,i)
+
+perechi(_,[],[]).
+perechi(E,[H|T],[[E|H]|LP]):-perechi(E,T,LP).
+
+%determina lista de perechi
+%listaAux(L:lista,LP:lista,LF:lista)
+%L - lista de numere
+%LP - lista partiala de perechi
+%LF - lista finala de perechi
+%model de flux (i,i,o), (i,i,i)
+listaAux([],LP,LP).
+listaAux([H|T],LP,LF):-perechi(H,T,L),append(LP,L,R),listaAux(T,R,LF).
+
+%lista(L:lista,LF:lista)
+%L - lista de numere
+%LF - lista de perechi
+%model de flux (i,o), (i,i)
+
+lista(L,LF):-listaAux(L,[],LF).
