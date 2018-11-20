bcac9503d197f62777d202534b63e36e05fb9b32
Teofana <teofanaenachioiu@yahoo.com>
2018-11-20 12:01:09 +0200

    Prolog

--- PLF/Test Prolog/p14.pl
+++ PLF/Test Prolog/p14.pl
@@ -1,27 +1,11 @@
-%invers(L,R)
 invers([],Col,Col):-!.
-invers([H|T],Col, R):-Coll=[H|Col],invers(T,Coll,R).
+invers([H|T],Col,R):-Coll=[H|Col],invers(T,Coll,R).
 
+adun([],_,0,[]):-!.
+adun([],_,Trans,[Trans]):-!.
+adun([H|T],N,Trans,[Nr|R]):-
+    Nr is mod(H+N+Trans,10),
+    Trans1 is div(H+N+Trans,10),
+    adun(T,0,Trans1,R).
 
-adunareAux([],[],1,[1]):-!.
-adunareAux([],[],0,[]):-!.
-adunareAux([H|T],[],Trans,[Nr|R]):-
-    Nr is mod(H+Trans,10),
-    Trans1 is div(H+Trans,10),!,
-    adunareAux(T,[],Trans1,R).
-adunareAux([],[H|T],Trans,[Nr|R]):-
-    Nr is mod(H+Trans,10),
-    Trans1 is div(H+Trans,10),!,
-    adunareAux([],T,Trans1,R).
-adunareAux([H1|T1],[H2|T2],Trans,[Nr|R]):-
-    Nr is mod(H1+H2+Trans,10),
-    Trans1 is div(H1+H2+Trans,10),!,
-    adunareAux(T1,T2,Trans1,R).
-
-%adunare(L1,L2,R)
-
-adunare(L1,L2,R):-
-    invers(L1,[],R1),
-    invers(L2,[],R2),
-    adunareAux(R1,R2,0,Rez),
-    invers(Rez,[],R).
+adunare(L,N,R):-invers(L,[],Li),adun(Li,N,0,Rp),invers(Rp,[],R).
