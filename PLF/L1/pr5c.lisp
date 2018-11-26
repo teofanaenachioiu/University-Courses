c524e49c214d9bbb4aa921657ac485c12105da03
Teofana <teofanaenachioiu@yahoo.com>
2018-11-26 15:53:56 +0200

    problema 5 c

--- PLF/L1/pr5c.lisp
+++ PLF/L1/pr5c.lisp
@@ -3,8 +3,6 @@
 ;;; lista si calculeaza numarul zecimal 
 ;;; corespunzator sumei.
 
-;;; NU MERGE. DE CE NU MERGEEEEEEEEEE ?
-
 (defun invers (L)
 	(invers_aux L NIL)
 	)
@@ -29,18 +27,18 @@
 		(
 			(NULL L2)
 			(APPEND (CONS (MOD (+ (CAR L1) transp) 10) NIL) 
- 			(suma_aux (CDR L1) NIL (/ (+ (CAR L1) transp) 10)))
+ 			(suma_aux (CDR L1) NIL (floor (+ (CAR L1) transp) 10)))
 		)
 		(
 			(NULL L1)
 			(APPEND (CONS (MOD (+ (CAR L2) transp) 10) NIL) 
- 			(suma_aux NIL (CDR L2) (/ (+ (CAR L2) transp) 10)))
+ 			(suma_aux NIL (CDR L2) (floor (+ (CAR L2) transp) 10)))
 		)
 		(
 			T
 			(APPEND 
 				(CONS (MOD (+ (CAR L2) (CAR L1) transp) 10) NIL) 
- 			(suma_aux (CDR L1) (CDR L2) (/ (+ (CAR L1) (CAR L2) transp) 10)))
+ 			(suma_aux (CDR L1) (CDR L2) (floor (+ (CAR L1) (CAR L2) transp) 10)))
 		)
 	)
 )
@@ -50,3 +48,58 @@
 		)
 )
 
+(defun sumaNrAux (L1 L2 transp deMul)
+	(cond
+		(
+			(AND (NULL L1) (NULL L2) (= transp 0))
+			0
+			)
+		(
+			(AND (NULL L1) (NULL L2) (= transp 1))
+			deMul
+			)
+		(
+			(NULL L2)
+			(+ 
+				(* deMul (MOD (+ (CAR L1) transp) 10))
+				(sumaNrAux 
+					(CDR L1)
+					NIL 
+					(floor (+ (CAR L1) transp) 10)
+					(* deMul 10)
+				)
+				
+				)
+			)
+		(
+			(NULL L1)
+			(+
+				(* deMul (MOD (+ (CAR L2) transp) 10))
+				(sumaNrAux 
+					NIL 
+					(CDR L2) 
+					(floor (+ (CAR L2) transp) 10)
+					(* deMul 10)
+					)
+				)
+				
+			)
+		(
+			T
+			(+
+				(* deMul (MOD (+ (CAR L1)(CAR L2) transp) 10))
+				(sumaNrAux 
+					(CDR L1) 
+					(CDR L2) 
+					(floor (+ (CAR L1) (CAR L2) transp) 10)
+					(* deMul 10)
+					)
+					
+			)
+		)
+	)
+	)
+
+(defun sumaNr (L1 L2)
+	(sumaNrAux (invers L1) (invers L2) 0 1) 
+	)
