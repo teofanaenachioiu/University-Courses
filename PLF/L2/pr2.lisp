8220b94ee0978c8ced914742920388b116f4e7b3
Teofana <teofanaenachioiu@yahoo.com>
2018-12-10 00:50:01 +0200

    L2 final

--- PLF/L2/pr2.lisp
+++ PLF/L2/pr2.lisp
@@ -1,8 +1,33 @@
-(defun arbore(L nrv nrm)
+(defun arboreDr(L nrv nrm)
 	(cond
-		((equal nrv (+ nrm 1)) (LIST L))
-		(T (APPEND (CONS (LIST (CAR L) NIL) (arbore (CDDR L) (+ nrv 1) (+ nrm (CADR L)) ))
+		((equal nrv (+ nrm 1))  L)
+		((NULL L) NIL)
+		(T (arboreDr (CDDR L) (+ nrv 1) (+ nrm (CADR L)) ))
+	)
+)
+
+(defun arboreStg(L nrv nrm)
+	(cond
+		((equal nrv (+ nrm 1)) NIL)
+		((NULL L) NIL)
+		(T (APPEND (LIST (CAR L) (CADR L)) 
+			(arboreStg (CDDR L)  (+ nrv 1) (+ nrm (CADR L)))
+			)	
+		)
+
+	)
+)
+
+(defun noduriNivel(L k)
+	(cond
+		((NULL L) NIL)
+		((equal k 1) (LIST (CAR L))) 
+		(T 
+			(APPEND (noduriNivel (arboreStg (CDDR L) 0 0) (- k 1)) 
+				(noduriNivel (arboreDr (CDDR L) 0 0) (- k 1)))
+			)
+		
+
 		)
 	)
-	)
-	) 
+	 
