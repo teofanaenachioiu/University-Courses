8ec5e8f4123a7b6c1e022f15d92d52bbe1efd3d6
Teofana <teofanaenachioiu@yahoo.com>
2019-01-24 23:45:28 +0200

    examen scris

--- Databases/Examen scris/query.sql
+++ Databases/Examen scris/query.sql
@@ -8,6 +8,7 @@ INSERT INTO R VALUES(1,2,3);
 INSERT INTO R VALUES(1,2,4);
 INSERT INTO R VALUES(3,3,5);
 
+
 /* Display all the records from the table */
 
 CREATE TABLE S(A integer, D integer);
@@ -18,3 +19,32 @@ INSERT INTO S VALUES(3,8);
 
 SELECT DISTINCT R.B FROM R LEFT OUTER JOIN S ON R.B=S.A
 SELECT DISTINCT S.A FROM R RIGHT OUTER JOIN S ON R.C=S.D
+
+INSERT INTO R VALUES(10,3,3);
+INSERT INTO S VALUES(11,3);
+
+SELECT * FROM R INNER JOIN S ON R.A=S.A;
+SELECT * FROM R LEFT JOIN S ON R.A=S.A;
+
+
+select * from R
+union 
+select * from R
+
+
+create table T(a integer, b integer)
+
+insert into T(a,b)
+values
+(1,3),(1,1),(3,1),(null,4),(5,null)
+
+select * from T
+
+select a from T t1
+where exists(select * from T where a=t1.b)
+
+select a from T
+where b= any (select a from T)
+
+insert into R(a,b)
+values (7,3)
