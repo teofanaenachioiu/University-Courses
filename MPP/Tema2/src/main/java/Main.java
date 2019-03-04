79e2ddf67958b5a52277e2033dbf2da81876c7bf
Teofana <teofanaenachioiu@yahoo.com>
2019-03-04 10:36:55 +0200

    tama 2

--- MPP/Tema2/src/main/java/Main.java
+++ MPP/Tema2/src/main/java/Main.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- MPP/Tema2/src/main/java/repository/IRepository.java
+++ MPP/Tema2/src/main/java/repository/IRepository.java
@@ -1,4 +1,10 @@
 package repository;
 
-public class IRepository {
+public interface IRepository<ID, T> {
+    int size();
+    void save(T entity);
+    void delete(ID id);
+    void update(ID id, T entity);
+    T findOne(ID id);
+    Iterable<T> findAll();
 }
