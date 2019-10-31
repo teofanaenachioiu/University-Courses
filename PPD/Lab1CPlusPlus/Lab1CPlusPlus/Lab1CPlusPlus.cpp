7647b881a2f74574a17d77106035490890606bf5
teofanaenachioiu <teofanaenachioiu@yahoo.com>
2019-10-31 10:53:40 +0200

    inmultirea in c++

--- PPD/Lab1CPlusPlus/Lab1CPlusPlus/Lab1CPlusPlus.cpp
+++ PPD/Lab1CPlusPlus/Lab1CPlusPlus/Lab1CPlusPlus.cpp
@@ -11,33 +11,29 @@
 
 int main()
 {
-	int no_th = 4;
+	int no_th = 8;
 	srand(time(NULL));
-	//Utils::createNewFile("input.txt", 2, 100000, 100100);
+	//Utils::createNewFile("input.txt", 2, 100, 1100);
 
 	vector<BigNumber> bigNumbers = Utils::readBigNumbersFromFile("input.txt");
 
 	BigNumber bn1 = bigNumbers.at(0);
 	BigNumber bn2 = bigNumbers.at(1);
 
-	auto starttime = chrono::steady_clock::now();
-	BigNumber sequentialmultiply = bn1.multiplySequentially(bn2);
+
+	/*auto starttime = chrono::steady_clock::now();
+	bn1.multiplySequentially(bn2);
 	auto endtime = chrono::steady_clock::now();
-	Utils::writeBigNumberInFile(sequentialmultiply, "seqvential.txt");
 
 	auto diff = endtime - starttime;
 	cout << "time: " << chrono::duration<double, milli>(diff).count() << endl;
-	sequentialmultiply.printBigNumber();
+*/
 
 	auto startTime1 = chrono::steady_clock::now();
-	BigNumber parallelMultiply  = bn1.multiplyParallel(bn2,no_th);
+	bn1.multiplyParallel(bn2,no_th);
 	auto endTime1 = chrono::steady_clock::now();
-	Utils::writeBigNumberInFile(parallelMultiply, "parallel.txt");
 
 	auto diff1 = endTime1 - startTime1;
 	cout << "Time: " << chrono::duration<double, milli>(diff1).count() << endl;
-	parallelMultiply.printBigNumber();
 
-	bool res = Utils::compareFiles("seqvential.txt", "parallel.txt");
-	cout << res << endl;
 }
