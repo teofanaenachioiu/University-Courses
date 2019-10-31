52f06e8c33e86a11f262b2af26c049124dab185b
teofanaenachioiu <teofanaenachioiu@yahoo.com>
2019-10-31 10:55:02 +0200

    inmultirea in Java

--- PPD/Lab1Java/src/main/java/Main.java
+++ PPD/Lab1Java/src/main/java/Main.java
@@ -4,28 +4,25 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        int minDigits = 100_000;
-        int maxDigits = 100_100;
+//        int minDigits = 10_000;
+//        int maxDigits = 10_100;
 //        Utils.createNewFile("num.txt", 2, minDigits, maxDigits);
-        int no_threads = 8;
+        int no_threads = 4;
 
         List<BigNumber> numbers = Utils.readNumbersFromFile("num.txt");
         BigNumber bigNumber1 = numbers.get(0);
         BigNumber bigNumber2 = numbers.get(1);
-        BigNumber bigNumberSec;
-        BigNumber bigNumberPar;
 
         long start, finish;
         float time;
 
 
-        start = System.nanoTime();
-        bigNumber1.multiplySequentially(bigNumber2);
-        finish = System.nanoTime();
-        time = (finish - start) / (float) 1_000_000;
-        System.out.println("Time: " + time + " sequential");
+//        start = System.nanoTime();
+//        bigNumber1.multiplySequentially(bigNumber2);
+//        finish = System.nanoTime();
+//        time = (finish - start) / (float) 1_000_000;
+//        System.out.println("Time: " + time + " sequential");
 
-//        Utils.writeNumberInFile(bigNumberSec, "sequential.txt");
 
         long start1, finish1;
         float time1;
@@ -35,10 +32,5 @@ public class Main {
         finish1 = System.nanoTime();
         time1 = (finish1 - start1) / (float) 1_000_000;
         System.out.println("Time: " + time1 + " parallel");
-//        Utils.writeNumberInFile(bigNumberPar, "parallel.txt");
-
-//        boolean isSame = Utils.isSameContentInFile("sequential.txt", "parallel.txt");
-//        System.out.println(isSame);
-
     }
 }
